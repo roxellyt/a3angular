76ed55ecbc3bbfa80e726b0aabd786ddbfb9acf8
roxellyt <roxelly.tavares@gmail.com>
2022-06-24 20:14:05 -0300

    1 file modified

--- src/app/roxelly-list-component/roxelly-list-component.component.ts
+++ src/app/roxelly-list-component/roxelly-list-component.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { RoxellyListServiceService } from '../roxelly-list-service.service';
 
 @Component({
   selector: 'app-roxelly-list-component',
   templateUrl: './roxelly-list-component.component.html',
-  styleUrls: ['./roxelly-list-component.component.css']
+  styleUrls: ['./roxelly-list-component.component.css'],
 })
 export class RoxellyListComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public roxellyService: RoxellyListServiceService;
+
+  updateBitcoinRates() {
+    this.roxellyService.update();
   }
-
 }
