6332a246fe4d389a99ba0a1eafd015ce215a7d8a
roxellyt <roxelly.tavares@gmail.com>
2022-06-24 19:41:33 -0300

    1 file modified, 4 added

---
+++ src/app/roxelly-home-component/roxelly-home-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roxelly-home-component',
+  templateUrl: './roxelly-home-component.component.html',
+  styleUrls: ['./roxelly-home-component.component.css']
+})
+export class RoxellyHomeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/roxelly-list-component/roxelly-list-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roxelly-list-component',
+  templateUrl: './roxelly-list-component.component.html',
+  styleUrls: ['./roxelly-list-component.component.css']
+})
+export class RoxellyListComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
