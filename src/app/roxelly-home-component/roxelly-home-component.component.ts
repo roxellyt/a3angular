b47d19eb936daa74d673eef9af3f5c2b9ccea90f
roxellyt <roxelly.tavares@gmail.com>
2022-06-24 21:07:03 -0300

    2 files modified

--- src/app/roxelly-home-component/roxelly-home-component.component.ts
+++ src/app/roxelly-home-component/roxelly-home-component.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-roxelly-home-component',
   templateUrl: './roxelly-home-component.component.html',
-  styleUrls: ['./roxelly-home-component.component.css']
+  styleUrls: ['./roxelly-home-component.component.css'],
 })
 export class RoxellyHomeComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
